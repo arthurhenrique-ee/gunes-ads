839ee050d3ed268ca59e3da8ca609a7ea5a8a892
Arthur Henrique Gunes <art123.gunesmarciano@gmail.com>
2026-07-20 21:32:41 -0300

    Update auth.php

--- server/auth.php
+++ server/auth.php
@@ -2,6 +2,14 @@
     session_start();
 
     if ($_SESSION["id"]) {
-        // ...
+        $id = $_SESSION["id"];
+        $nome = $_SESSION["nome"];
+        $primeiroNome = explode(" ", $nome)[0];
+        $telefone = $_SESSION["telefone"];
+        $email = $_SESSION["email"];
+        $senha = $_SESSION["senha"];
+    } else {
+        header("location: ../index.php");
+        exit;
     }
 ?>
